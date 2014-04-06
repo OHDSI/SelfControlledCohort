bfae33b6b9ad351f844946e6b5aa348de80d47eb
unknown <PRyan4@WPRDUSR98RGGCR.wks.jnj.com>
2014-04-06 05:50:04 -0400

    updated SQL

---
+++ R/SelfControlledCohort.R
@@ -0,0 +1,68 @@
+# @file SelfControlledCohort.R
+#
+# Copyright 2014 Observational Health Data Sciences and Informatics
+#
+# This file is part of SelfControlledCohort
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# @author Observational Health Data Sciences and Informatics
+# @author Martijn Schuemie
+# @author Patrick Ryan
+
+#' @title selfControlledCohort
+#'
+#' @description
+#' \code{selfControlledCohort} generates population-level estimation from OMOP CDMv4 instance by comparing exposed and unexposed time among exposed cohort.
+#'
+#' @details
+#' PATRICK HOMEWORK:   complete details
+#' 
+#' 
+#' @param connectionDetails  An R object of type ConnectionDetail (details for the function that contains server info, database type, optionally username/password, port)
+#' @param cdmSchema			string name of databsae schema that contains OMOP CDM and vocabulary
+#' @param resultsSchema		string name of database schema that we can write results to
+#' @param sourceName		string name of the database, as recorded in results
+#' @param analysisIds		(optional) a vector containing the set of OSCAR analysisIds for which results will be generated.
+#' If not specified, all analyses will be executed.
+#' 
+#' @return An object of type \code{sccResults} containing details for connecting to the database containing the results 
+#' @examples \dontrun{
+#'   connectionDetails <- createConnectionDetails(dbms="sql server", server="RNDUSRDHIT07.jnj.com")
+#'   oscarResults <- oscar(connectionDetails, "cdm4_sim", "scratch", "TestDB")
+#'   plot(oscarResults, "population")
+#' }
+#' @export
+selfControlledCohort <- function (connectionDetails, cdmSchema, resultsSchema, sourceName, analysisIds){
+  
+  pathToSql <- system.file("sql", "SccParameterizedSQL.sql", package="SelfControlledCohort")
+  parameterizedSql <- readChar(pathToSql,file.info(pathToSql)$size)
+  
+  renderedSql <- renderSql(parameterizedSql[1], CDM_schema = cdmSchema, results_schema = resultsSchema, source_name = sourceName)$sql
+  
+  
+  conn <- connectUsingConnectionDetails(connectionDetails)
+  
+  writeLines("Executing large query. This could take a while")
+  dbSendUpdate(conn,renderedSql)
+  writeLines(paste("Done. Results can now be found in",resultsSchema))
+  
+  dbDisconnect(conn)
+  
+  resultsConnectionDetails <- connectionDetails
+  resultsConnectionDetails$schema = resultsSchema
+  
+  result <- list(resultsConnectionDetails = resultsConnectionDetails, call = match.call())
+  class(result) <- "sccResults"
+  result
+}
