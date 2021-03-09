77a1cd8a928caaedc80ba017a380df805a8b7d92
jgilber2 <JGIlber2@its.jnj.com>
2021-03-09 13:17:23 -0800

    Moved common database settings to setup.R file

--- tests/testthat/test-RunAnalyses.R
+++ tests/testthat/test-RunAnalyses.R
@@ -15,34 +15,6 @@ test_that("multiple analyses", {
                                     runSelfControlledCohortArgs = runSelfControlledCohortArgs2)
   sccAnalysisList <- list(sccAnalysis1, sccAnalysis2)
 
-  if (getOption("dbms") == "postgresql") {
-    connectionDetails <- createConnectionDetails(dbms = "postgresql",
-                                                 user = Sys.getenv("CDM5_POSTGRESQL_USER"),
-                                                 password = URLdecode(Sys.getenv("CDM5_POSTGRESQL_PASSWORD")),
-                                                 server = Sys.getenv("CDM5_POSTGRESQL_SERVER"))
-
-    cdmDatabaseSchema <- Sys.getenv("CDM5_POSTGRESQL_CDM_SCHEMA")
-    cdmVersion <- 5
-  }
-  if (getOption("dbms") == "sql server") {
-    connectionDetails <- createConnectionDetails(dbms = "sql server",
-                                                 user = Sys.getenv("CDM5_SQL_SERVER_USER"),
-                                                 password = URLdecode(Sys.getenv("CDM5_SQL_SERVER_PASSWORD")),
-                                                 server = Sys.getenv("CDM5_SQL_SERVER_SERVER"))
-    cdmDatabaseSchema <- Sys.getenv("CDM5_SQL_SERVER_CDM_SCHEMA")
-    cdmVersion <- 5
-  }
-  if (getOption("dbms") == "oracle") {
-    connectionDetails <- createConnectionDetails(dbms = "oracle",
-                                                 user = Sys.getenv("CDM5_ORACLE_USER"),
-                                                 password = URLdecode(Sys.getenv("CDM5_ORACLE_PASSWORD")),
-                                                 server = Sys.getenv("CDM5_ORACLE_SERVER"))
-    cdmDatabaseSchema <- Sys.getenv("CDM5_ORACLE_CDM_SCHEMA")
-    oracleTempSchema <- Sys.getenv("CDM5_ORACLE_OHDSI_SCHEMA")
-
-    cdmVersion <- 5
-  }
-
   outputFolder <- file.path(tempdir(), getOption("dbms"))
   dir.create(outputFolder)
   on.exit(unlink(outputFolder))
--- tests/testthat/test-parameterSweep.R
+++ tests/testthat/test-parameterSweep.R
@@ -54,32 +54,5 @@ testAllParams <- function(connectionDetails,
 }
 
 test_that("SCC", {
-  if (getOption("dbms") == "postgresql") {
-    connectionDetails <- createConnectionDetails(dbms = "postgresql",
-                                                 user = Sys.getenv("CDM5_POSTGRESQL_USER"),
-                                                 password = URLdecode(Sys.getenv("CDM5_POSTGRESQL_PASSWORD")),
-                                                 server = Sys.getenv("CDM5_POSTGRESQL_SERVER"))
-
-    cdmDatabaseSchema <- Sys.getenv("CDM5_POSTGRESQL_CDM_SCHEMA")
-    cdmVersion <- 5
-  }
-  if (getOption("dbms") == "sql server") {
-    connectionDetails <- createConnectionDetails(dbms = "sql server",
-                                                 user = Sys.getenv("CDM5_SQL_SERVER_USER"),
-                                                 password = URLdecode(Sys.getenv("CDM5_SQL_SERVER_PASSWORD")),
-                                                 server = Sys.getenv("CDM5_SQL_SERVER_SERVER"))
-    cdmDatabaseSchema <- Sys.getenv("CDM5_SQL_SERVER_CDM_SCHEMA")
-    cdmVersion <- 5
-  }
-  if (getOption("dbms") == "oracle") {
-    connectionDetails <- createConnectionDetails(dbms = "oracle",
-                                                 user = Sys.getenv("CDM5_ORACLE_USER"),
-                                                 password = URLdecode(Sys.getenv("CDM5_ORACLE_PASSWORD")),
-                                                 server = Sys.getenv("CDM5_ORACLE_SERVER"))
-    cdmDatabaseSchema <- Sys.getenv("CDM5_ORACLE_CDM_SCHEMA")
-    oracleTempSchema <- Sys.getenv("CDM5_ORACLE_OHDSI_SCHEMA")
-
-    cdmVersion <- 5
-  }
   testAllParams(connectionDetails, cdmDatabaseSchema, cdmVersion)
 })
