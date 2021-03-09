95f1350ca2664d773df952dcae088aa211358eee
jgilber2 <JGIlber2@its.jnj.com>
2021-03-09 11:43:38 -0800

    Runs runAnalyses across real databases

--- tests/testthat/test-RunAnalyses.R
+++ tests/testthat/test-RunAnalyses.R
@@ -1,6 +1,7 @@
+library(testthat)
 
 test_that("multiple analyses", {
-
+  # Analysis.R is checked elsewhere
   exposureOutcome1 <- createExposureOutcome(767410, 444382)
   exposureOutcome2 <- createExposureOutcome(1314924, 444382)
   exposureOutcome3 <- createExposureOutcome(907879, 444382)
@@ -14,11 +15,49 @@ test_that("multiple analyses", {
                                     runSelfControlledCohortArgs = runSelfControlledCohortArgs2)
   sccAnalysisList <- list(sccAnalysis1, sccAnalysis2)
 
+  if (getOption("dbms") == "postgresql") {
+    connectionDetails <- createConnectionDetails(dbms = "postgresql",
+                                                 user = Sys.getenv("CDM5_POSTGRESQL_USER"),
+                                                 password = URLdecode(Sys.getenv("CDM5_POSTGRESQL_PASSWORD")),
+                                                 server = Sys.getenv("CDM5_POSTGRESQL_SERVER"))
+
+    cdmDatabaseSchema <- Sys.getenv("CDM5_POSTGRESQL_CDM_SCHEMA")
+    cdmVersion <- 5
+  }
+  if (getOption("dbms") == "sql server") {
+    connectionDetails <- createConnectionDetails(dbms = "sql server",
+                                                 user = Sys.getenv("CDM5_SQL_SERVER_USER"),
+                                                 password = URLdecode(Sys.getenv("CDM5_SQL_SERVER_PASSWORD")),
+                                                 server = Sys.getenv("CDM5_SQL_SERVER_SERVER"))
+    cdmDatabaseSchema <- Sys.getenv("CDM5_SQL_SERVER_CDM_SCHEMA")
+    cdmVersion <- 5
+  }
+  if (getOption("dbms") == "oracle") {
+    connectionDetails <- createConnectionDetails(dbms = "oracle",
+                                                 user = Sys.getenv("CDM5_ORACLE_USER"),
+                                                 password = URLdecode(Sys.getenv("CDM5_ORACLE_PASSWORD")),
+                                                 server = Sys.getenv("CDM5_ORACLE_SERVER"))
+    cdmDatabaseSchema <- Sys.getenv("CDM5_ORACLE_CDM_SCHEMA")
+    oracleTempSchema <- Sys.getenv("CDM5_ORACLE_OHDSI_SCHEMA")
+
+    cdmVersion <- 5
+  }
+
   outputFolder <- tempdir()
-  rr <- runSccAnalyses(connectionDetails = Eunomia::getEunomiaConnectionDetails(),
-                       cdmDatabaseSchema = "main",
+  rr <- runSccAnalyses(connectionDetails = connectionDetails,
+                       cdmDatabaseSchema = cdmDatabaseSchema,
+                       oracleTempSchema = oracleTempSchema,
                        sccAnalysisList = sccAnalysisList,
                        exposureOutcomeList = exposureOutcomeList,
                        outputFolder = outputFolder,
                        computeThreads = 8)
+
+  expect_is(rr, "data.frame")
+  expect_true(file.exists(file.path(outputFolder, "resultsReference.rds")))
+  apply(rr, 1, function(item) {
+    expect_true(file.exists(file.path(outputFolder, item["sccResultsFile"])))
+  })
+
+  result <- summarizeAnalyses(rr, outputFolder)
+  expect_is(result, "data.frame")
 })
