2b891bbd48d773d88f297328966691dafcd5014d
jgilber2 <JGIlber2@its.jnj.com>
2021-03-09 17:20:05 -0800

    Database connector jar files

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,3 +1,16 @@
+# Download the JDBC drivers used in the tests
+
+oldJarFolder <- Sys.getenv("DATABASECONNECTOR_JAR_FOLDER")
+Sys.setenv("DATABASECONNECTOR_JAR_FOLDER" = tempfile("jdbcDrivers"))
+DatabaseConnector::downloadJdbcDrivers("postgresql")
+DatabaseConnector::downloadJdbcDrivers("sql server")
+DatabaseConnector::downloadJdbcDrivers("oracle")
+
+withr::defer({
+  unlink(Sys.getenv("DATABASECONNECTOR_JAR_FOLDER"), recursive = TRUE, force = TRUE)
+  Sys.setenv("DATABASECONNECTOR_JAR_FOLDER" = oldJarFolder)
+}, testthat::teardown_env())
+
 if (getOption("dbms") == "postgresql") {
   connectionDetails <- createConnectionDetails(dbms = "postgresql",
                                                user = Sys.getenv("CDM5_POSTGRESQL_USER"),
